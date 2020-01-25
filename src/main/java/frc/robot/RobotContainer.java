4836ca1d7ec5df664441c3712e0a0908ff28027e
ROBODORES <robodores4255official@gmail.com>
2020-01-25 15:36:27 -0800

    updatedRobotContainer

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -9,12 +9,12 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.XboxController.Button;
 import edu.wpi.first.wpilibj.XboxController;
 import frc.robot.subsystems.Climber;
 import frc.robot.subsystems.Conveyor;
 import frc.robot.subsystems.Drivetrain;
 import frc.robot.subsystems.ExampleSubsystem;
-import frc.robot.subsystems.FrontIntake;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.commands.*;
@@ -29,15 +29,14 @@ public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
   private final Drivetrain m_drivetrain = new Drivetrain();
-  //private final Climber m_climber = new Climber();
-  //private final FrontIntake m_frontIntake = new FrontIntake();
-  //private final Conveyor m_conveyor = new Conveyor();
+  private final Climber m_climber = new Climber();
+  private final Conveyor m_conveyor = new Conveyor();
 
   private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
   //private final Test m_test = new Test(m_conveyor, m_frontIntake);
   //public static Deploy deploy = new Deploy();
 
-  public static Joystick controller;
+  /*public static Joystick controller;
   public static JoystickButton aButton;
   public static JoystickButton bButton;
   public static JoystickButton xButton;
@@ -47,29 +46,31 @@ public class RobotContainer {
   public static JoystickButton leftMiniButton;
   public static JoystickButton rightMiniButton;
   public static JoystickButton leftStickButton;
-  public static JoystickButton rightStickButton;
+  public static JoystickButton rightStickButton;*/
 
-  public static XboxController xController;
+  public static XboxController m_xController;
 
   /**
    * The container for the robot.  Contains subsystems, OI devices, and commands.
    */
   public RobotContainer() {
     // Configure the button bindings
-    controller = new Joystick(0);
-    /*aButton = new JoystickButton(controller, 1);
+    /*controller = new Joystick(0);
+    aButton = new JoystickButton(controller, 1);
     bButton = new JoystickButton(controller, 2);
     xButton = new JoystickButton(controller, 3);
     yButton = new JoystickButton(controller, 4);
-    leftBumperButton = new JoystickButton(controller, 5);*/
-    //rightBumperButton = new JoystickButton(controller, 6);
-    /*leftMiniButton = new JoystickButton(controller, 7);
+    leftBumperButton = new JoystickButton(controller, 5);
+    rightBumperButton = new JoystickButton(controller, 6);
+    leftMiniButton = new JoystickButton(controller, 7);
     rightMiniButton = new JoystickButton(controller, 8);
     leftStickButton = new JoystickButton(controller, 9);
     rightStickButton = new JoystickButton(controller, 10);
     xController = new XboxController(0);*/
 
     configureButtonBindings();
+
+    m_drivetrain.setDefaultCommand(new Drive(m_drivetrain));
   }
 
   /**
@@ -79,7 +80,8 @@ public class RobotContainer {
    * {@link edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
   private void configureButtonBindings() {
-    //new JoystickButton(controller, 6).whileHeld(m_test);//anger
+    new JoystickButton(m_xController, Button.kBumperRight.value).whenPressed(new Intake(m_conveyor)); //Intake
+    new JoystickButton(m_xController, Button.kX.value).whenPressed(new Deploy(m_conveyor));
   }
 
 
